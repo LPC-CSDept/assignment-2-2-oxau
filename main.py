92ac3006e2acb1231818dd8f767be6362c1a654e
oxau <notoriousact@gmail.com>
2023-02-06 23:55:15 -0800

    xoxo

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    celsius = float(input('Temperature in Celsius: '))
+    fahrenheit = float(9/5*(celsius))+32
+
+    print ('Temperature in Fahrenheit: ' + '{0:.2f}'. format (fahrenheit))
     pass
 
 
